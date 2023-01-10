5d79c5abc275b2201f50f6d5851184530d2bc98a
unknown <>
2023-01-10 22:30:04 +0900

    env 파일 추가

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyARBue5ozz3A2aSsP1fsIEp83yhNCJB_Oc",
+  authDomain: "nwitter-2ccef.firebaseapp.com",
+  projectId: "nwitter-2ccef",
+  storageBucket: "nwitter-2ccef.appspot.com",
+  messagingSenderId: "682572881108",
+  appId: "1:682572881108:web:b3dde5f2caa67166832f41"
+};
+
+export default firebase.initializeApp(firebaseConfig);
