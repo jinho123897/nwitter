5f9bd931c6f035dbece69815cf46b25de4283e31
unknown <>
2023-01-10 22:36:05 +0900

    env 수정

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase/app'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyARBue5ozz3A2aSsP1fsIEp83yhNCJB_Oc",
-  authDomain: "nwitter-2ccef.firebaseapp.com",
-  projectId: "nwitter-2ccef",
-  storageBucket: "nwitter-2ccef.appspot.com",
-  messagingSenderId: "682572881108",
-  appId: "1:682572881108:web:b3dde5f2caa67166832f41"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 export default firebase.initializeApp(firebaseConfig);
