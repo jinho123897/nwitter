a2c50a7459a97158e980b0730631d3a40ff7b1e6
unknown <>
2023-01-10 23:31:04 +0900

    firebase auth 설정

--- src/fbase.js
+++ src/fbase.js
@@ -1,4 +1,7 @@
-import * as firebase from 'firebase/app'
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -9,4 +12,6 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID
 };
 
-export default firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+export const authService = firebase.auth();
