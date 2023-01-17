aebbc3c4f51cca2b2a98e3cf2c315d57ce15f676
unknown <>
2023-01-17 23:29:19 +0900

    firebase 회원 가입 및 로그인 업데이트

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,52 @@
-import React from 'react'
+import { authService } from 'fbase';
+import React, { useState } from 'react'
 
 export default function Auth() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
+
+  const onChange = (event) => {
+    const {target: {name, value}} = event;
+    
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  }
+
+  const onSubmit = async(event) => {
+    event.preventDefault();
+    let data;
+    try {
+      if(newAccount){
+        data = await authService.createUserWithEmailAndPassword( email, password );
+      }else{  
+        data = await authService.signInWithEmailAndPassword( email, password );
+      }
+    } catch (error) {
+      setError(error.message);
+    } 
+  };
+
+  const toggleAccount = () => setNewAccount(prev => !prev);
+
   return (
-    <div>Auth</div>
-  )
+    <div>
+      <form onSubmit={onSubmit}>
+        <input name="email" type="email" placeholder="Email" required  value={email} onChange={onChange}/>
+        <input name="password" type="password" placeholder="Password" required  value={password} onChange={onChange}/>
+        <input type="submit" value={newAccount ? "Create Account" : "Sign In"} />
+        {error}
+      </form>
+      <span onClick={toggleAccount}>{newAccount ? "Sign In." : "Create Account"}</span>
+      <div>
+          <button name="google">Continue with Google</button>
+          <button name="github">Continue with Github</button>
+      </div>
+    </div>
+  );
 }
