18f3fcebd6212bc0412465cb93787859f9c32b7e
unknown <>
2023-01-25 23:46:48 +0900

    글 삭제 및 수정 기능 추가

--- src/components/App.js
+++ src/components/App.js
@@ -21,7 +21,7 @@ function App() {
 
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+      {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} /> : "Initializing..."}
       <footer>&copy; {new Date().getFullYear()} nwitter</footer>
     </>
     
